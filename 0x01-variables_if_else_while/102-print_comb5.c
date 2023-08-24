b60fbe409f7f64b5077ba398d3bc1af73cb4efa2
PhilipAkei <akeikurphilipdeng@gmail.com>
2023-08-24 01:23:25 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
-/**
- * main - prints all possible combinations of pair of 
- * two digit numbers, without any repetition.
- (*
-*Return: 0 on success.
-*/
 
+/**
+ * main - Prints all possible combinations of three different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int i, j;
-	for (i = 0; i <= 99; i++)
-	{ 
-		for (j = 0; j <= 99; j+++)
-			if (i < &&&ni != j)
+	int digit1, digit2, digit3;
+
+	for (digit1 = 0; digit1 < 8; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+		{
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
-				putchar((i / 10) + '0');
-				putchar((i % 10) + '0');
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
+
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					continue;
+				
+				putchar(',');
 				putchar(' ');
-				putchar((j / 10) + '0');
-				putchar((j % 10) + '0');
-				if (i != 98 || j != 99)
-				{ 
-					putchar(',')
-					putchar(',')
-				}
 			}
+		}
 	}
-        putchar('\n');
+
+	putchar('\n');
+
 	return (0);
 }
