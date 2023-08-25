a9f5c7aeb214feff17a30ef5321ddbbab3d06325
PhilipAkei <akeikurphilipdeng@gmail.com>
2023-08-25 00:13:23 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper -prog allowe check for uppercase 
+ * @c: variable text
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else 
+{
+return (0);
+}
+}
