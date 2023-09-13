921d2a0e1965ae6a2bd728ffde715b84db3d389a
PhilipAkei <akeikurphilipdeng@gmail.com>
2023-09-13 02:43:28 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- * print_name -  Print a name from a function pointer h
+ * print_name - Print a name from a function pointer h
  * @name: char string
  * @f: function pointer h that takes a string argument 
  */
